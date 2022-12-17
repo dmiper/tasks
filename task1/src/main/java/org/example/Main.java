ad7494f8f836b16f9306ba7a08c08c25b4845812
dmiper <pervushin687@gmail.com>
2022-12-17 23:49:05 +0300

    Update

--- task1/src/main/java/org/example/Main.java
+++ task1/src/main/java/org/example/Main.java
@@ -1,9 +1,17 @@
 package org.example;
 
 public class Main {
-    public static void main(String[] args) {
+public static void main(String[] args) {
 
+    int randomDirection = (int) (Math.random() * 3 + 1);
+    int startX = (int) (Math.random() * 100 - 10);
+    int startY = (int) (Math.random() * 100 - 10);
+    int finishX = (int) (Math.random() * 100 - 10);
+    int finishY = (int) (Math.random() * 100 - 10);
+
+    Robot robot = switch (randomDirection) {
         case 1 -> new Robot(startX, startY, Direction.DOWN);
+        case 2 -> new Robot(startX, startY, Direction.RIGHT);
         case 3 -> new Robot(startX, startY, Direction.UP);
         case 4 -> new Robot(startX, startY, Direction.LEFT);
         default -> throw new IllegalStateException("Unexpected value: " + randomDirection);
